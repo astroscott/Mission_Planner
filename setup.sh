411f045d0473afdd0fffaa8009a0388d14f3f1c6
astroscott <aaronmst@gmail.com>
2021-08-28 20:01:47 -0700

    Setup updated

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+
+[theme]
+primaryColor='#F63366'\n\
+backgroundColor='#FFFFFF'\n\
+secondaryBackgroundColor='#F0F2F6'\n\
+textColor='#262730'\n\
+font='sans serif'\n\
 " > ~/.streamlit/config.toml
