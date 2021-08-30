a3adf34991009b39d47570ae75b95132743fc662
astroscott <aaronmst@gmail.com>
2021-08-30 12:36:47 -0700

    updated config and heroku setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
-mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-
-[theme]
-primaryColor='#F63366'\n\
-backgroundColor='#FFFFFF'\n\
-secondaryBackgroundColor='#F0F2F6'\n\
-textColor='#262730'\n\
-font='sans serif'\n\
 " > ~/.streamlit/config.toml
