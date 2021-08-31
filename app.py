b2ebb4390e3fa10a70d7609ddbc0e08773fb241f
astroscott <aaronmst@gmail.com>
2021-08-31 13:26:04 -0700

    prepped for streamlit hosting

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ Body1 = Planet(config["ab"]) # Arrival Body
 (rP0, vP0, rP1, vP1, c3s0, c3l0, vinfs1, vinfl1, dVs, dVl, tofs) = Tools.Transfer.solve(config["k"], Body0, Body1, jdd, jad)
 
 # Create Plot
+config["plt_title"] = "Mission: " + config["db"] + " to " + config["ab"] + " " + str(dd[0].tolist().year) + ", Type 1, 2 Transfers"
 st.title(config["plt_title"])
 st.write("By [Aaron Scott](https://www.linkedin.com/in/aaron-scott-899797216/), visit the [GitHub repository](https://www.github.com/astroscott/Mission_Planner) for full source code.")
-config["plt_title"] = "Mission: " + config["db"] + " to " + config["ab"] + " " + str(dd[0].tolist().year) + ", Type 1, 2 Transfers"
 st.plotly_chart(Tools.Plot.porkchop(config, dd, ad, c3s0, c3l0, vinfs1, vinfl1, dVs, dVl, tofs), use_container_width=True)
